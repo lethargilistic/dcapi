7df04cfa048539bf9d82e20c62bab6f130b9e28b
lethargilistic <mikeoverby@outlook.com>
2017-04-30 21:51:20 -0400

    Move build into build.py

--- dcapi.py
+++ dcapi.py
@@ -5,6 +5,7 @@ from weppy.tools import service
 from weppy.orm import Database, Model, Field
 from weppy.sessions import SessionCookieManager
 
+from build import build_all
 from keys import DB_PIPE_KEY
 from models import Character
 
@@ -13,37 +14,9 @@ app = App(__name__)
 db = Database(app, auto_migrate=True)
 db.define_models(Character)
 
-def build_characters():
-    with open('data/json/characters.json', 'r') as datafile:
-        characters_dict = json.loads(datafile.read())
-
-    Character.all().delete()
-    for char in characters_dict:
-        parameters = dict(
-            id=char['id'],
-            kanji_name=char['name']['japanese']['kanji'],
-            romanized_name=char['name']['japanese']['romanized'],
-            english_anime_name=char['name']['english']['anime'],
-            english_manga_name=char['name']['english']['manga'],
-            age=char['age'],
-            date_of_birth=char['date_of_birth'],
-            gender='Female' if char['gender'] == 'F' else 'Male',
-            occupation=char['occupation'],
-            manga_first_appearance=char['first_appearance']['manga'],
-            anime_first_appearance=char['first_appearance']['anime'],
-            cases_solved=char['cases_solved'],
-            keyhole=char['keyhole'],
-            japanese_voice=char['voice']['japanese'],
-            english_voice=char['voice']['english'],
-            drama_actor=char['drama_actor'],
-        )
-        Character.create(**parameters)
-
-    db.commit()
-
 @app.command('setup')
 def setup():
-    build_characters()
+    build_all(db)
 
 app.pipeline = [
     SessionCookieManager(DB_PIPE_KEY), db.pipe
